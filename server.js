4cf06c9d1e89021dd24c3310c5c14308390bb66e
Dans7an <kdanstanmark@gmail.com>
2021-12-09 18:38:15 -0500

    created entry

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const server = express();
 
 server.use(express.json());        // to parse json data  
 server.use(express.urlencoded({ extended: true })) // use express.urlencoded to parse incoming requests with payloads
+server.set("view engine", "ejs")    // allows us to use the ejs files in the view folder
 
+server.get("/entry", (req,res) => {
+    res.render("entry.ejs")
+})
 
 server.listen(process.env.PORT || 3000, () => {
     console.log("Listening on port 3000");
