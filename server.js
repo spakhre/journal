d14a4bfd8f17776ad3ff1c8329323c1a31af6f8f
spakhre <sushmap.sde@gmail.com>
2021-12-09 15:19:55 -0800

    journal

--- server.js
+++ server.js
@@ -2,6 +2,31 @@ const express = require("express");
 
 const server = express();
 
+const mongoose = require("mongoose");
+const Entry = require("./models/journal");
+
+const connectionString = 'mongodb+srv://dbUser:Journal2021@cluster0.jdzse.mongodb.net/Journal-Entry?retryWrites=true&w=majority';
+
+const userRoutes = require(".routes/routes.js")
+
+//routes for all
+server.use(userRoutes)
+
+mongoose.connect(connectionString, 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("DB connection successful");
+    }).catch((error) => {
+        console.log("connection error:", error);
+    })
+
+server.get('/', (req, res)=>{
+    const journal = new Entry({title: 'Today' })
+    res.send(journal);
+})
+
 server.use(express.json());        // to parse json data  
 server.use(express.urlencoded({ extended: true })) // use express.urlencoded to parse incoming requests with payloads
 
