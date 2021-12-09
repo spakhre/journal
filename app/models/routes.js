247456ebe3c53c5fa6163cf17ba3bf5ecc6e7151
anvytran-dev <anvytran857@gmail.com>
2021-12-09 17:13:09 -0500

    started passport

---
+++ app/models/routes.js
@@ -0,0 +1,22 @@
+module.exports = function (app, passport, db) {
+
+ // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function (req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+
+
+}
