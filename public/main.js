a188b984d90e296967d029921b0d8ca52d3615d5
Dans7an <kdanstanmark@gmail.com>
2021-12-10 18:05:27 -0500

    added auth middleware && delete functionality

--- public/main.js
+++ public/main.js
@@ -9,3 +9,24 @@ function editEntry(e) {
     console.log(e.target)
     
 }
+
+let deleteButton = document.getElementsByClassName('deleteButton');
+
+Array.from(deleteButton).forEach( function(element){
+    element.addEventListener('click', function(event){
+        const entryId = event.target.getAttribute('entry-id');
+        const userId = event.target.getAttribute('user-id');
+
+        fetch(`allEntries`,{
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                "userId": userId,
+                "entryId": entryId
+            })
+        })
+        .then((res) => {
+            window.location.reload()
+        })
+    })
+    })
